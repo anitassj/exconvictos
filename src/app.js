69203a99827967d41e4aba7633d06d1a6283aae3
catvidela <katherinevidela530@gmail.com>
2024-08-21 01:54:41 -0300

    Commit de prueba después de reiniciar el índice

--- src/app.js
+++ src/app.js
@@ -4,22 +4,47 @@ const app = express();
 const path = require('path');
 const port = 3000;
 
+const bodyParser = require("body-parser");
+
+const conexion = require("./conexion"); //requerimos la ruta para manejar la conexion
+
 // middleware 
 app.use(express.urlencoded({
     extended: false,
 }));
 app.use(express.json());
 
-app.use('/public', express.static("../public"));
+//bodyparser es un middl que permite acceder y procesar datos enviados por form html
+app.use(bodyParser.json());
+
+//analiza datos codificados por url
+app.use(bodyParser.urlencoded({extended: false}));
+
+//para servir archivos estaticos desde la carpeta public
+app.use('/public', express.static(path.join(__dirname, "public")));
+
+//para establecer ejs como el motor de plantillas ,INSTALAR NPM INSTALL EJS
+app.set("view engine", "ejs");
+app.set('views',path.join(__dirname,"public"));
 
 // rutas
 const rutaLogin = require('./routes/login');
-const rutaInicio = require('./routes/index');
+
+const rutaInicio = require('./routes/inicio');
+
 
 app.use('/', rutaLogin);
-app.use('/', rutaInicio);
+app.use('/inicio', rutaInicio);
+
+//DEFINIENDO LA RUTA INICIO!!!!! IMPORTANTE
+/*app.get("/", (req, res) => {
+    res.render("inicio", {
+      rutaActual: "/",
+    });
+  });*/
+  
 
 // levantar el servidor 
 app.listen(port, () => {
     console.log(`El servidor corre en el puerto ${port}`);
-});
+})
---
+++ src/routes/form.js
@@ -0,0 +1,51 @@
+//backend del formulario de datos del preso
+const express = require('express');
+const router = express.Router();
+const db = require('../conexion'); //para importar la conexion a la db
+
+  
+//ruta formulario 
+/*app.get("/form", (req, res) => {
+    res.render("pages/form", {
+      rutaActual: "/form",
+    });
+});*/
+    app.post("/procesar-formulario", async (req, res) => {
+        console.log(req.body);
+        // Verificar campos vacíos
+        for (const campo in req.body) {
+          if (!req.body[campo]) {
+            res.send(`Error: El campo ${campo} está vacío.`);
+            return;
+          }
+        }
+      
+        /**
+         * Desestructuración de los datos del body
+         */
+        const { nombre, apellido, dni, fecha_nac, ciudad_natal, ciudad_prox, num_causa, abogado, juzgado } = req.body;
+        try {
+          // Realizar la inserción en la base de datos
+          const query =
+            "INSERT INTO exconvictos_datos (nombre, apellido, dni, fecha_nac, ciudad_natal, ciudad_prox, num_causa, abogado, juzgado) VALUES (?, ?, ?, ?)";
+          await connection.execute(query, [
+            nombre, 
+            apellido, 
+            dni, 
+            fecha_nac, 
+            ciudad_natal, 
+            ciudad_prox, 
+            abogado, 
+            juzgado
+          ]);
+      
+          res.render("inicio", {
+            rutaActual: "/",
+          });
+          //res.send(`¡Formulario procesado correctamente!`);
+        } catch (error) {
+          console.error("Error al insertar en la base de datos: ", error);
+          console.log(error); // Agregar esta línea para imprimir el error completo en la consola
+          res.send("Error al procesar el formulario");
+        }
+      });
