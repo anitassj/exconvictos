2e4ad7a0655a76597cd74495e9b16a18c8200a0f
catvidela <katherinevidela530@gmail.com>
2024-08-30 00:33:16 -0300

    arreglo variable node

--- src/routes/guardar_datos.js
+++ src/routes/guardar_datos.js
@@ -5,19 +5,19 @@ const conexion = require('../models/db');
 
 // Ruta para procesar el formulario
 router.post('/guardar-datos', (req, res) => {
-    const { tipo, patente, date, marca, modelo, nombre, apellido, celular, email } = req.body;
+    const { tipo, patente, anio, marca, modelo, nombre, apellido, celular, email } = req.body;
     //hacemos una consulta para guardar los datos del formulario en la db, se coloca ? tantos datos haya para mas seguridad
     console.log('Datos recibidos:', req.body); //para ver q me esta llegando pq no se guarda el anio
 
 
      // Verifica que elnio se haya convertido a número
-     if (isNaN(date)) {
+     if (isNaN(anio)) {
         return res.status(400).json({ error: 'Año inválido. Por favor ingrese un número válido para el año.' });
     }
     
     conexion.query(
          'INSERT INTO  solicitante_form (tipo, patente, anio, marca, modelo,nombre,apellido,celular,email) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
-         [tipo,patente,date,marca,modelo,nombre,apellido,celular,email],
+         [tipo,patente,anio,marca,modelo,nombre,apellido,celular,email],
         (error, results) => {
         if (error) { //si existe un error
             return res.status(400).json({ error: 'Error al procesar los datos.' }); //validacion
