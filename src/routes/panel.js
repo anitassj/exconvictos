648e9c352cc2e84ef29b63721ab1c956ed5bcbf5
juannleiva <juanmartin.liyo@gmail.com>
2024-08-30 00:39:22 -0300

    2

---
+++ src/routes/panel.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const path = require('path'); 
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.render(path.join(__dirname, '..', 'views', 'panel.ejs'));
+});
+
+module.exports = router;
