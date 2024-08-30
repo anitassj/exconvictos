4043495d7d0c2ae67f0f47cfcf9a1e9d415c3623
catvidela <katherinevidela530@gmail.com>
2024-08-30 01:12:46 -0300

    arreglo de ruta

--- src/routes/panel.js
+++ src/routes/panel.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path'); 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/panel', (req, res) => {
     res.render(path.join(__dirname, '..', 'views', 'panel.ejs'));
 });
 
