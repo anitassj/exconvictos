81526622777d4576cd6c0ddb1278a5c198f047d8
anitassj <analuzulariaga@gmail.com>
2024-08-30 21:55:43 -0300

    div carrusel

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -7,3 +7,27 @@ document.addEventListener("DOMContentLoaded", function() {
         menuResponsive.classList.toggle("activo");
     });
 });
+
+// Carrusel ----------------
+document.addEventListener('DOMContentLoaded', function () {
+  var swiper = new Swiper('.swiper-container', {
+    speed: 1000,
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: true, 
+    effect: 'coverflow', 
+    autoplay: {
+      delay: 4000, 
+      disableOnInteraction: false, 
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets', 
+      clickable: true,
+    }
+  });
+});
