cb49a450721af792f6924b759c551d08863a1d12
catvidela <katherinevidela530@gmail.com>
2024-09-01 12:40:46 -0300

    cambio de rutas, y tuve q ajustar las variables y methodpost del htlm login

--- src/models/usuario_modelo.js
+++ src/models/usuario_modelo.js
@@ -6,8 +6,8 @@ class UsuarioModel {
 		return new Promise((resolve, reject) => {
 			let sql = `SELECT * FROM datos_usuarios WHERE email = ? AND clave = ?`;
 			
-			console.log('Ejecutando consulta SQL:', sql); /*para ver si se esta ejecutando la consulta*/
-            console.log('Con parámetros:', [email, clave]); /*y ver los parametros q esta recibiendo*/
+			console.log('Ejecutando consulta SQL:', sql); /*para ver si se esta ejecutando la consulta...*/
+            console.log('Con parámetros:', [email, clave]); /*y ver los parametros q esta recibiendo.*/
 
 			conexion.query(sql, [email, clave], (err, results) => { //el código ejecuta la consulta SQL contra la base de datos utilizando la conexión conx. los valores email y password se pasan como parámetros para evitar inyecciones sql.
 				try {
