86d1cbea2629db7305e8a00bb49238273305ee53
catvidela <katherinevidela530@gmail.com>
2024-09-15 13:23:27 -0300

    cambios campos form

--- src/routes/guardar_datos.js
+++ src/routes/guardar_datos.js
@@ -5,10 +5,10 @@ const conexion = require('../models/db');
 
 // Ruta para procesar el formulario
 router.post('/guardar-datos', (req, res) => {
-    const { tipo, patente, anio, marca, modelo, nombre, apellido, celular, email } = req.body;
-    if (!tipo || !patente || !anio || !marca || !modelo || !nombre || !apellido || !celular || !email) {
+    const { tipo, patente, anio, id_marca, id_modelo, nombre, apellido, celular, email } = req.body;
+    if (!tipo || !patente || !anio || !id_marca || !id_modelo || !nombre || !apellido || !celular || !email) {
         return res.status(400).json({ error: 'No se completaron todos los campos' });
-    } //VALIDACION PARA QUE COMPLETEN TODOS LOS CAMPOS
+    }//VALIDACION PARA QUE COMPLETEN TODOS LOS CAMPOS
 
     //hacemos una consulta para guardar los datos del formulario en la db, se coloca ? tantos datos haya para mas seguridad
     console.log('Datos recibidos:', req.body); //para ver q me esta llegando pq no se guarda el anio
@@ -21,7 +21,7 @@ router.post('/guardar-datos', (req, res) => {
     
     conexion.query(
          'INSERT INTO  solicitante_form (tipo, patente, anio, marca, modelo,nombre,apellido,celular,email) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
-         [tipo,patente,anio,marca,modelo,nombre,apellido,celular,email], 
+         [tipo,patente,anio,id_marca, id_modelo,nombre,apellido,celular,email], 
         (error, results) => {
         if (error) { //si existe un error
             return res.status(400).json({ error: 'Error al procesar los datos.' }); //validacion
