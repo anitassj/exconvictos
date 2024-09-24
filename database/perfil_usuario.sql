a19e58316c9eae3e626694f288769ca649dfef32
notclaru <mariaclaramartinez20@gmail.com>
2024-09-24 13:56:38 -0300

    hice la tabla para perfil usuario (database), datos de prueba  y archivos en carpetas models, controllers, y routes

--- database/perfil_usuario.sql
+++ database/perfil_usuario.sql
@@ -11,11 +11,15 @@ provincia varchar(100),
 tipo_vehiculo varchar(100),
 patente varchar(10),
 anio varchar(4),
-vigencia_desde varchar(50),
-vigencia_hasta varchar(50),
+vigencia_desde date,
+vigencia_hasta date,
 fotos varchar(255),
 tipo_seguro varchar(100),
 premio_total varchar(100),
 suma_asegurada varchar(100),
 uso_vehiculo varchar(100)
 );
+
+insert into perfil_usuario(nombre, apellido, email, celular, direccion, ciudad, provincia, tipo_vehiculo, patente, anio, vigencia_desde, vigencia_hasta, fotos, tipo_seguro, premio_total, suma_asegurada, uso_vehiculo)values("Clara", "Martínez", "cm@mail.com", 0303456, "Pellegrini 7", "Chivilcoy", "Buenos Aires", "Auto", "cc777cc", 2007, "07/05/2024", "07/05/2025", "www.google.com", "premium", 777777, 7777777, "personal");
+
+select * from perfil_usuario;
