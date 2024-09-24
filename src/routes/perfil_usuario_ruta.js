0fa0900c9a6bd8f348aa0e822b74079b4e971184
notclaru <mariaclaramartinez20@gmail.com>
2024-09-24 14:45:26 -0300

    borré codigo que no funciona

--- src/routes/perfil_usuario_ruta.js
+++ src/routes/perfil_usuario_ruta.js
@@ -1,8 +1,1 @@
-const express = require('express');
-const router = express.Router();
-const perfilUsuarioController = require('../controllers/perfil_usuario_controller');
 
-// ruta para mostrar el perfil de usuario
-router.get('/perfil_usuario/:id', perfilController.mostrarPerfil);
-
-module.exports = router;
