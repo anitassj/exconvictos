a48c2ce6eaaabc14f291e3d7689a79707a16ef60
notclaru <mariaclaramartinez20@gmail.com>
2024-09-24 14:43:43 -0300

    borré codigo que no funciona

--- src/routes/perfil_usuario_ruta.js
+++ src/routes/perfil_usuario_ruta.js
@@ -3,19 +3,6 @@ const router = express.Router();
 const perfilUsuarioController = require('../controllers/perfil_usuario_controller');
 
 // ruta para mostrar el perfil de usuario
-router.get('/:id', perfilUsuarioController.mostrarPerfil);
-
-// rutas para los botones
-router.post('/:id/emitir', (req, res) => {
-  // aca iria la lógica para emitir una póliza
-});
-
-router.post('/:id/editar', (req, res) => {
-  // aca iria la lógica para editar el perfil
-});
-
-router.post('/:id/guardar', (req, res) => {
-  // aca iria la lógica para guardar los cambios
-});
+router.get('/perfil_usuario/:id', perfilController.mostrarPerfil);
 
 module.exports = router;
