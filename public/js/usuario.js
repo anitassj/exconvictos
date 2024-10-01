a2e4a5808e1264d38a85352119d7c37f4088e520
Benja797 <benjamerchan7@gmail.com>
2024-10-01 09:11:13 -0300

    panel de usuario

--- public/js/usuario.js
+++ public/js/usuario.js
@@ -52,8 +52,28 @@ const menu = document.querySelector('.menu');
 const iconoBarras = document.querySelector('#icono-barras');
 const iconoX = document.querySelector('#icono-x');
 
+// secciones
+const vehiculosAseguradosA = document.querySelector('#vehiculos-aseg');
+const datosPersonalesA = document.querySelector('#datos-pers');
+const denunciarSiniestroA = document.querySelector('#denunciar-sini');
+
+const datosPersonalesSection = document.querySelector('.datos-personales');
+const vehiculosAseguradosSection = document.querySelector('.vehiculos-asegurados');
+
 menuHamburguesa.addEventListener('click', () => {
     menu.classList.toggle('mostrar');
     iconoX.classList.toggle('mostrar');
     iconoBarras.classList.toggle('ocultar');
 });
+
+datosPersonalesA.addEventListener('click', () => {
+    datosPersonalesSection.classList.add('mostrar');
+    vehiculosAseguradosSection.classList.add('ocultar');
+    menu.classList.toggle('mostrar');
+})
+
+vehiculosAseguradosA.addEventListener('click', () => {
+    vehiculosAseguradosSection.classList.remove('ocultar');
+    datosPersonalesSection.classList.remove('mostrar');
+    menu.classList.toggle('mostrar');
+})
