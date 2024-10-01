c91b01586ed770c679afbeddd45d97b88b4bd970
catvidela <katherinevidela530@gmail.com>
2024-10-01 16:30:13 -0300

    cargr cliente ruta

--- src/models/cargar-clienteModel.js
+++ src/models/cargar-clienteModel.js
@@ -3,23 +3,16 @@ const conexion = require('../models/db');
 class cargarCliente {
 
     static async guardarDatos(cargarCliente) {
-        const {nombre, apellido, dni, email, celular, direccion, ciudad, provincia, tipo_vehiculo, patente, anio, vigencia_desde, vigencia_hasta, foto, tipo_seguro, premio_total, suma_asegurada, uso_vehiculo } =  cargarCliente;
+        const {nombre, apellido, dni, email, celular, direccion, ciudad, provincia, tipo_vehiculo, patente, anio, vigencia_desde, vigencia_hasta, foto, tipo_seguro, premio_total, suma_asegurada, uso_vehiculo } = cargarCliente;
         
         try {
         const sql = `
-            INSERT INTO datosPersonales
-            (nombre, apellido, dni, email, celular, direccion, ciudad, provincia) 
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?)
-        `;
+            INSERT INTO datosPersonales(nombre, apellido, dni, email, celular, direccion, ciudad, provincia) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
         const [results]= await conexion.promise().query(sql, [nombre, apellido, dni, email, celular, direccion, ciudad, provincia]);
         //para obtener el ID del cliente insertado, para futuras consultas
         const id_cliente = results.insertId;
         
-        const sql2 = `
-        INSERT INTO datosVehiculo
-        (id_cliente, tipo_vehiculo, patente, anio, vigencia_desde, vigencia_hasta, foto, tipo_seguro, premio_total, suma_asegurada, uso_vehiculo)
-        VALUES (?,?,?,?,?,?,?,?,?,?,?)
-        `;
+        const sql2 = `INSERT INTO datosVehiculo(id_cliente, tipo_vehiculo, patente, anio, vigencia_desde, vigencia_hasta, foto, tipo_seguro, premio_total, suma_asegurada, uso_vehiculo) VALUES (?,?,?,?,?,?,?,?,?,?,?)`;
         const [results2]= await conexion.promise().query(sql2, [id_cliente, tipo_vehiculo, patente, anio, vigencia_desde, vigencia_hasta, foto, tipo_seguro, premio_total, suma_asegurada, uso_vehiculo]);
        
         //devuelve un objeto con los datos resultados de cada consulta
