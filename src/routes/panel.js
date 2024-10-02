de1be22cdcd833df8b3a2ea6d0720e4bffabefb0
notclaru <mariaclaramartinez20@gmail.com>
2024-10-02 12:39:29 -0300

    cerrar sesion en el panel, listo.

--- src/routes/panel.js
+++ src/routes/panel.js
@@ -8,4 +8,15 @@ const panelController = new controllerPanel();
 
 router.get('/panel', validarUsuario, panelController.mostrarListado);
 
+router.get('/cerrar-sesion', (req, res) => {
+    req.session.destroy((err) => {
+        if (err) {
+            console.log('Error al cerrar sesión:', err); 
+            return res.status(500).send('Error al cerrar sesión');
+        }
+
+        res.redirect('/login');
+    });
+});
+
 module.exports = router;
