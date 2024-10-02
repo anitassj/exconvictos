8bf60ff9773d7567253e4dc0ae812f1d9077aed3
catvidela <katherinevidela530@gmail.com>
2024-10-02 16:40:54 -0300

    ultimos cambios

--- src/models/cargar-clienteModel.js
+++ src/models/cargar-clienteModel.js
@@ -1,28 +1,30 @@
-const conexion = require('../models/db');
+const conexion = require('../models/db'); 
 
-class cargarCliente { 
+class CargarCliente {
+    static async guardarDatos(clienteData) {
+        const { nombre, apellido, dni, email, celular, direccion, ciudad, provincia } = clienteData;
+        const { tipo_vehiculo, patente, anio, vigencia_desde, vigencia_hasta, foto, tipo_seguro, premio_total, suma_asegurada, uso_vehiculo } = clienteData;
 
-    static async guardarDatos(cargarCliente) {
-        const {nombre, apellido, dni, email, celular, direccion, ciudad, provincia, tipo_vehiculo, patente, anio, vigencia_desde, vigencia_hasta, foto, tipo_seguro, premio_total, suma_asegurada, uso_vehiculo } = cargarCliente;
-        
         try {
-        const sql = `
-            INSERT INTO datosPersonales(nombre, apellido, dni, email, celular, direccion, ciudad, provincia) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
-        const [results]= await conexion.promise().query(sql, [nombre, apellido, dni, email, celular, direccion, ciudad, provincia]);
-        //para obtener el ID del cliente insertado, para futuras consultas
-        const id_cliente = results.insertId;
-        
-        const sql2 = `INSERT INTO datosVehiculo(id_cliente, tipo_vehiculo, patente, anio, vigencia_desde, vigencia_hasta, foto, tipo_seguro, premio_total, suma_asegurada, uso_vehiculo) VALUES (?,?,?,?,?,?,?,?,?,?,?)`;
-        const [results2]= await conexion.promise().query(sql2, [id_cliente, tipo_vehiculo, patente, anio, vigencia_desde, vigencia_hasta, foto, tipo_seguro, premio_total, suma_asegurada, uso_vehiculo]);
-       
-        //devuelve un objeto con los datos resultados de cada consulta
-        return { clienteDatos: results, vehiculoDatos: results2 };
+            const sql = `
+                INSERT INTO datosPersonales(nombre, apellido, dni, email, celular, direccion, ciudad, provincia) 
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
+            const [results] = await conexion.promise().query(sql, [nombre, apellido, dni, email, celular, direccion, ciudad, provincia]);
+
+            const id_cliente = results.insertId;
+
+            const sql2 = `
+                INSERT INTO datosVehiculo(id_cliente, tipo_vehiculo, patente, anio, vigencia_desde, vigencia_hasta, foto, tipo_seguro, premio_total, suma_asegurada, uso_vehiculo) 
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+            const [results2] = await conexion.promise().query(sql2, [id_cliente, tipo_vehiculo, patente, anio, vigencia_desde, vigencia_hasta, foto, tipo_seguro, premio_total, suma_asegurada, uso_vehiculo]);
+
+            return { clienteDatos: results, vehiculoDatos: results2 };
 
         } catch (error) {
-            console.error('error en la consulta:', error);
-            throw error;
+            console.error('Error en la consulta:', error);
+            throw error; 
         }
     }
-};
+}
 
-module.exports = cargarCliente;
+module.exports = CargarCliente;
