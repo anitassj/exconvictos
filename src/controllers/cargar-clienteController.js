ef2450445657c04a8481e18c4d5de3e547e4bbd2
catvidela <katherinevidela530@gmail.com>
2024-10-03 02:24:01 -0300

    ARREGLO FINAAAAAAAAAAAAAAAAAAL cargar-cliente

--- src/controllers/cargar-clienteController.js
+++ src/controllers/cargar-clienteController.js
@@ -2,25 +2,39 @@ const cargarCliente = require('../models/cargar-clienteModel');
 
 class cargarClienteController {
     static async guardarDatos(req, res) {
-         try {
-           const {nombre, apellido, dni, email, celular, direccion, ciudad, provincia, tipo_vehiculo, patente, anio, vigencia_desde, vigencia_hasta, tipo_seguro, premio_total, suma_asegurada, uso_vehiculo} = req.body;
-            
-           //: null para ver que exista req.file y no sea undefined
-           const foto = req.file ? "/" + req.file.path : null;
+        try {
+            console.log('Datos recibidos:', req.body);
 
-           const resultado = await cargarCliente.guardarDatos({
-               nombre, apellido ,dni,email,celular,direccion,ciudad,provincia,tipo_vehiculo,patente,anio,vigencia_desde,vigencia_hasta,foto,tipo_seguro,premio_total,suma_asegurada,uso_vehiculo
-           });
+            //pasear los datos pq llegan comocadenas , esto lo convierte en objetos (sin esto tira error reference u is not valid json )
+            const datosPersonales = JSON.parse(req.body.datosPersonales);
+            const datosVehiculo = JSON.parse(req.body.datosVehiculo);
 
-           res.status(201).json({
-               message: 'Datos guardados ',
-               data: resultado 
-           });
-       } catch (error) {
-           console.error('Error al guardar los datos:', error);
-           res.status(500).json({ error: 'Error al guardar los datos' });
-       }
-   }
+            const foto = req.file ? "/" + req.file.path : null;
+
+            //destructurar los datos que recibo, segun los metodos para el error not definido
+            const {
+                nombre, apellido, dni, email, celular, direccion, ciudad, provincia
+            } = datosPersonales;
+
+            const {
+                tipo_vehiculo, patente, anio, vigencia_desde, vigencia_hasta, tipo_seguro, premio_total, suma_asegurada, uso_vehiculo
+            } = datosVehiculo;
+
+            const resultado = await cargarCliente.guardarDatos({
+                nombre, apellido, dni, email, celular, direccion, ciudad, provincia,
+                tipo_vehiculo, patente, anio, vigencia_desde, vigencia_hasta, foto, tipo_seguro, premio_total, suma_asegurada, uso_vehiculo
+            });
+
+            res.status(201).json({
+                message: 'Datos guardados',
+                data: resultado 
+            });
+        } catch (error) {
+            console.error('Error al guardar los datos:', error);
+            res.status(500).json({ error: 'Error al guardar los datos' });
+        }
+    }
 };
+
 
 module.exports = cargarClienteController;
