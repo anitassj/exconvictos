421e6a399a50b9af76e7bc126b86a4a7dc722224
notclaru <mariaclaramartinez20@gmail.com>
2024-10-08 15:24:29 -0300

    validacion para usuario ya existente, lista.

--- src/controllers/crear_cliente-controlador.js
+++ src/controllers/crear_cliente-controlador.js
@@ -5,9 +5,15 @@ const crearUsuarioController = async (req, res) => {
 
     try {
         await crearUsuario({ nombre, apellido, dni, email, clave, rol_id });
+        console.log('Controlador: usuario creado exitosamente');
         res.json({ message: 'Usuario creado exitosamente' });
     } catch (error) {
         console.error('Error al crear el usuario:', error);
+
+        if (error.message.includes('Este usuario ya ha sido creado')) {
+            return res.status(400).json({ error: 'Este usuario ya ha sido creado.' });
+        }
+
         res.status(500).json({ error: 'Error en el servidor' });
     }
 };
