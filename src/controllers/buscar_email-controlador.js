2433e87cfdb81848efac9194706ea8af6726f4ce
notclaru <mariaclaramartinez20@gmail.com>
2024-10-08 10:48:46 -0300

    buscar email, nombre y apellido: listo.

--- src/controllers/buscar_email-controlador.js
+++ src/controllers/buscar_email-controlador.js
@@ -4,16 +4,19 @@ exports.buscarEmailPorDni = async (req, res) => {
     const { dni } = req.params; // Extraer el DNI de los parámetros de la URL
     try {
         const [result] = await conexion.promise().query(
-            'SELECT email FROM datosPersonales WHERE dni = ?',
+            'SELECT email, nombre, apellido FROM datosPersonales WHERE dni = ?',
             [dni]
         );
         
+        console.log('Resultado completo:', result); // Verifica el objeto completo
+
         if (result.length > 0) {
-            res.json({ 
-                email: result[0].email,
-                nombre: result[0].nombre, 
-                apellido: result[0].apellido
-             });
+            const cliente = result[0];
+            res.json({
+                email: cliente.email,
+                nombre: cliente.nombre,
+                apellido: cliente.apellido
+            });
         } else {
             res.status(404).json({ error: 'No se encontró el cliente con ese DNI' });
         }
