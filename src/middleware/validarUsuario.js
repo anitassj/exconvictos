06ffe85101ad8ebcd0fca56141ee7b62baa04148
notclaru <mariaclaramartinez20@gmail.com>
2024-10-09 13:23:58 -0300

    ajuste de logica de middleware

--- src/middleware/validarUsuario.js
+++ src/middleware/validarUsuario.js
@@ -1,13 +1,14 @@
 const validarUsuario = (req,res, next)=>{
     
+
+    if(!req.session.idUsuario){
+        return res.redirect('/login');
+    }
+    
     if (req.session.usuario !== undefined) {
         if (req.session.usuario.rol_id == 2) {
             return res.redirect('/usuario/datospersonales');
         }
-    }
-
-    if(!req.session.idUsuario){
-        return res.redirect('/login');
     }
 
     next();
