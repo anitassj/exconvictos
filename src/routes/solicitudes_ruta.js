a4bb2fd7659115a09565d89a45a0f95f4363ccbb
notclaru <mariaclaramartinez20@gmail.com>
2024-10-09 13:12:45 -0300

    incluyendo middleware en rutas pt.2

--- src/routes/solicitudes_ruta.js
+++ src/routes/solicitudes_ruta.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const solicitudesController = require('../controllers/solicitudes_controller');
+const validarUsuario = require('../middleware/validarUsuario');
 const controller = new solicitudesController();
 
-router.get('/solicitudes', controller.mostrarDatos); 
-router.get('/solicitudes/:id', controller.mostrarFormulario); 
-router.put('/solicitudes/:id/marcar-leido', controller.marcarComoLeido.bind(controller));
+router.get('/solicitudes', validarUsuario, controller.mostrarDatos); 
+router.get('/solicitudes/:id', validarUsuario, controller.mostrarFormulario); 
+router.put('/solicitudes/:id/marcar-leido', validarUsuario, controller.marcarComoLeido.bind(controller));
 
 
 module.exports = router;
