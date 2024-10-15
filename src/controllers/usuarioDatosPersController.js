ee0d471911fdea309690f9de7a5351b42f353e96
Benja797 <benjamerchan7@gmail.com>
2024-10-15 18:08:28 -0300

    para ver hoy a la tarde

--- src/controllers/usuarioDatosPersController.js
+++ src/controllers/usuarioDatosPersController.js
@@ -1,15 +1,25 @@
 // usuarioDatosPersController.js
-const { json } = require('express');
-const modelo = require('../models/usuarioDatosPersModel');
+const express = require('express');
+const router = express.Router();
+const DatosPersonalesModel = require('../models/usuarioDatosPersModel');
 
-const obtenerTodosLosDatos = (req, res) => {
-    modelo.obtenerDatosPersonales((error, datos) => {
-        if(error) {
-            return res.status(500).json( { error: 'Error al obtener los datos' } );
+// Controlador para obtener datos personales
+const obtenerDatosPersonales = async (req, res) => {
+    const idCliente = req.params.id_cliente;
+
+    try {
+        const datosPersonales = await DatosPersonalesModel.obtenerTodos(idCliente);
+        if (datosPersonales) {
+            return res.status(200).json(datosPersonales);
         } else {
-            return res.json(datos);
+            return res.status(404).json({ message: 'No se encontraron datos personales para este cliente.' });
         }
-    });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'Error interno del servidor.', error: error.message });
+    }
 };
 
-module.exports = { obtenerTodosLosDatos };
+router.get('/datos-personales/:id_cliente', obtenerDatosPersonales);
+
+module.exports = { obtenerDatosPersonales };
--- src/routes/usuarioDatosPersRuta.js
+++
@@ -1,9 +0,0 @@
-//usuarioDatosPersRuta.js
-const express = require('express');
-const router = express.Router();
-const usuarioDatosPersController = require('../controllers/usuarioDatosPersController');
-
-// Ruta para obtener todos los datos personales
-router.get('/usuario/datospersonales', usuarioDatosPersController.obtenerTodosLosDatos);
-
-module.exports = router;
