dcb373e2fdd2e7554e8f7c53821a90c61203c283
notclaru <mariaclaramartinez20@gmail.com>
2024-10-17 15:46:50 -0300

    arreglando detalles

--- src/controllers/perfil_usuario_controller.js
+++ src/controllers/perfil_usuario_controller.js
@@ -5,9 +5,10 @@ class PerfilUsuarioController {
         try {
             const dni = req.params.dni; 
             const usuario = await perfil_usuario_modelo.obtenerUsuarioPorDNI(dni); 
-
+            console.log('haciendo console log usuario:', usuario)
             if (usuario) {
-                res.render('perfil', { usuario: usuario }); 
+                res.render('perfil_usuario', { usuario }); 
+                console.log('imprimiendo desde el controlador', usuario);
             } else {
                 res.status(404).send('Usuario no encontrado');
             }
