2f4f00b0f8ddf6953927661bafbc2f3d8b399077
notclaru <mariaclaramartinez20@gmail.com>
2024-10-17 15:41:45 -0300

    .

--- src/routes/rutas-views/perfil_usuario_views.js
+++ src/routes/rutas-views/perfil_usuario_views.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path'); 
 const router = express.Router();
 
-router.get('/perfil_usuario/:dni', (req, res) => {
+router.get('/perfil_usuario', (req, res) => {
     res.render(path.join(__dirname, '..', '..', 'views', 'perfil.ejs'));
 });
 
