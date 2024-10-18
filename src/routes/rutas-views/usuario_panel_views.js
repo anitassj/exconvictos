d5168796a92b258eb21e8af214dfc2dbaae82236
Benja797 <benjamerchan7@gmail.com>
2024-10-18 18:05:04 -0300

    rutas!!

--- src/routes/rutas-views/usuario_panel_views.js
+++ src/routes/rutas-views/usuario_panel_views.js
@@ -3,7 +3,7 @@ const path = require('path');
 const router = express.Router();
 
 router.get('/usuario', (req, res) => {
-    res.render(path.join(__dirname, '..', '..', 'views', 'usuario_panel.ejs'));
+    res.redirect('/inicio-usuario');
 });
 
 module.exports = router;
